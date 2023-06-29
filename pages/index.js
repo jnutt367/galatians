9a4445a60baa3b321802f50d579e4170c0873c5f
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-06-28 22:40:00 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -46,12 +46,18 @@ export default function Home() {
                               />
        </a>
 
-         <a href="https://gofund.me/a2b590af">
-             <p className={styles.donate}>
-               Support <br />
-                  Creator
-               </p>
-       </a>
+         <a href="https://www.youtube.com/channel/UCC4jNk2Tms-KNO1h1YOjfRw">
+  <Image 
+                                src="/youtube-logo-png-hd-21.png"
+                                alt="youtube logo"
+                                 height={75}
+                                width={75}
+                                priority
+                              />
+          
+                                  
+                                  
+        </a>
          <a href="https://ephesians.vercel.app/">
            <Image 
                                 src="/chevron-right.svg"
