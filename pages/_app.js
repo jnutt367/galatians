95da9d5216563eac312cbcb918697a675174e099
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-07-03 21:44:11 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
+import { gsap } from 'gsap'
+import { CSSPlugin } from 'gsap/CSSPlugin'
+gsap.registerPlugin(CSSPlugin)
+  function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
